b7d754b06badfa986edca6ff91e05af12c7a9cf2
wahel31102005 <wahelsianturi684@gmail.com>
2024-08-01 11:30:28 +0700

    Upload

---
+++ LifestyleStore/connection.php
@@ -0,0 +1,4 @@
+<?php
+//$con=mysqli_connect("localhost","id2538044_sajalagrawal","","") or die(mysqli_error($con));
+$con=mysqli_connect("localhost","life_wahel332","wahel3445","life_lifestyle") or die(mysqli_error($con));
+?>
